190bd169ad79dca447af948c4c2bc2a66ab00d5e
Oskar <106465473+OsqaR0@users.noreply.github.com>
2022-09-24 19:08:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace if_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 10;
+            int n2 = 20;
+            {
+                Console.WriteLine("n2>n1");
+            }
+        }
+    }
+}
